8f80161d649814e1f3da992acc9b8a17d3b3fd59
Ayush Nigam <ayushnigamsworld@gmail.com>
2018-05-03 13:19:30 +0530

    TRANSIENT

---
+++ config/config.example.js
@@ -0,0 +1,8 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+const DB_USERNAME = 'root';
+const DB_PASSWORD = 'root';
+
+module.exports = {
+  db: `mongodb://${DB_USERNAME}:${DB_PASSWORD}@ds111420.mlab.com:11420/3cinnovation`,
+  db_dev: `mongodb://${DB_USERNAME}:${DB_PASSWORD}@ds111420.mlab.com:11420/3cinnovation`,
+};
