ecb9c04598594ad07eb7fd7c32ac98cf6f930163
daveranjan <dtiwari18@gmail.com>
2018-05-07 02:02:32 +0530

    Added Login Btn

--- client/app/components/Header/Carousal/Carousal.js
+++ client/app/components/Header/Carousal/Carousal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import GLoginBtn from './GLoginBtn';
 class Carousal extends Component {
 
     constructor(props){
@@ -18,10 +19,9 @@ class Carousal extends Component {
               <div className="carousel-item active">
                 <img className="d-block w-100 h-80" src="assets/img/slider/slide1.jpg" alt="First slide"/>
                 <div className="carousel-caption d-md-block">
-                  <p className="fadeInUp wow" data-wow-delay=".6s">Global Grand Event on Digital Design</p>
-                  <h1 className="wow fadeInDown heading" data-wow-delay=".4s">Design Thinking Conference</h1>
-                  <a href="#" className="fadeInLeft wow btn btn-common btn-lg" data-wow-delay=".6s">Get Ticket</a>
-                  <a href="#" className="fadeInRight wow btn btn-border btn-lg" data-wow-delay=".6s">Explore More</a>
+                  <p className="fadeInUp wow" data-wow-delay=".6s">3C Logic Innov8</p>
+                  <h1 className="wow fadeInDown heading" data-wow-delay=".4s">Hackathon Event</h1>
+                  <GLoginBtn/>
                 </div>
               </div>
               <div className="carousel-item">
--- client/app/components/Header/Nav/Nav.js
+++ client/app/components/Header/Nav/Nav.js
@@ -47,28 +47,10 @@ class Nav extends Component{
             <a className="page-scrool" href="#header-wrap">Home</a>
           </li>
           <li>
-            <a className="page-scrool" href="#about">About</a>
-          </li>
-          <li>
-             <a className="page-scroll" href="#schedules">schedules</a>
-          </li>
-          <li>
-            <a className="page-scroll" href="#team">Speakers</a>
-          </li>
-          <li>
-            <a className="page-scroll" href="#gallery">Gallery</a>
-          </li>
-          <li>
             <a className="page-scroll" href="#faq">Faq</a>
           </li>
           <li>
-            <a className="page-scroll" href="#sponsors">Sponsors</a>
-          </li>
-          <li>
-            <a className="page-scroll" href="#pricing">pricing</a>
-          </li>
-          <li>
-            <a className="page-scroll" href="#google-map-area">Contact</a>
+            <a className="page-scroll" href="#details">Details</a>
           </li>
         </ul>
 
