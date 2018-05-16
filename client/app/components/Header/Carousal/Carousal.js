40f49233a7165b7e004920eed53b97a2a74c413b
mkumar <mkumar@3clogic.com>
2018-05-16 15:53:46 +0530

    banner changes

--- client/app/components/Header/Carousal/Carousal.js
+++ client/app/components/Header/Carousal/Carousal.js
@@ -18,7 +18,7 @@ class Carousal extends Component {
             </ol>
             <div className="carousel-inner">
               <div className="carousel-item active">
-                <img className="d-block w-100 h-80" src="https://s3.amazonaws.com/3c-hackathon/slider/slide1.jpg" alt="First slide"/>
+                <img className="d-block w-100 h-80" src="https://s3.amazonaws.com/3c-hackathon/slider/inv2.jpg" alt="First slide"/>
                 <div className="carousel-caption d-md-block">
                 <h1 className="wow fadeInDown heading " data-wow-delay=".4s"><label className="ccc">3C Logic</label> <label className="innovation">inNovatiON</label></h1>
                 <p className="fadeInUp wow carousal-quote" data-wow-delay=".6s">"What good is an idea if it remains an idea? Try. Experiment. Iterate. Fail. Try again. Change the world."</p>
@@ -26,7 +26,7 @@ class Carousal extends Component {
                 </div>
               </div>
               <div className="carousel-item">
-                <img className="d-block w-100" src="https://s3.amazonaws.com/3c-hackathon/slider/slide2.jpg" alt="Second slide"/>
+                <img className="d-block w-100" src="https://s3.amazonaws.com/3c-hackathon/slider/slide1.jpg" alt="Second slide"/>
                 <div className="carousel-caption d-md-block">
                   <h1 className="wow bounceIn heading " data-wow-delay=".7s"><label className="ccc">3C Logic</label> <label className="innovation">inNovatiON</label></h1>
                   <p className="fadeInUp wow carousal-quote" data-wow-delay=".6s">"It always seems impossible until it's done."</p>
@@ -34,7 +34,7 @@ class Carousal extends Component {
                 </div>
               </div>
               <div className="carousel-item">
-                <img className="d-block w-100" src="https://s3.amazonaws.com/3c-hackathon/slider/slide3.jpg" alt="Third slide"/>
+                <img className="d-block w-100" src="https://s3.amazonaws.com/3c-hackathon/slider/slide2.jpg" alt="Third slide"/>
                 <div className="carousel-caption d-md-block">
                   <h1 className="wow fadeInUp heading  " data-wow-delay=".6s"><label className="ccc">3C Logic</label> <label className="innovation">inNovatiON</label></h1>
                   <p className="fadeInUp wow carousal-quote" data-wow-delay=".6s">"Innovation distinguishes between a leader and a follower."</p>
@@ -43,7 +43,7 @@ class Carousal extends Component {
               </div>
 
                <div className="carousel-item">
-                  <img className="d-block w-100" src="https://s3.amazonaws.com/3c-hackathon/slider/slide1.jpg" alt="Third slide"/>
+                  <img className="d-block w-100" src="https://s3.amazonaws.com/3c-hackathon/slider/slide3.jpg" alt="Third slide"/>
                   <div className="carousel-caption d-md-block">
                     <h1 className="wow fadeInUp heading  " data-wow-delay=".6s"><label className="ccc">3C Logic</label> <label className="innovation">inNovatiON</label></h1>
                     <p className="fadeInUp wow carousal-quote" data-wow-delay=".6s">"There’s a way to do it better—find it."</p>
@@ -51,7 +51,7 @@ class Carousal extends Component {
                   </div>
                </div>
                <div className="carousel-item">
-                  <img className="d-block w-100" src="https://s3.amazonaws.com/3c-hackathon/slider/slide2.jpg" alt="Third slide"/>
+                  <img className="d-block w-100" src="https://s3.amazonaws.com/3c-hackathon/slider/inv1.jpg" alt="Third slide"/>
                   <div className="carousel-caption d-md-block">
                      <h1 className="wow fadeInUp heading  " data-wow-delay=".6s"><label className="ccc">3C Logic</label> <label className="innovation">inNovatiON</label></h1>
                      <p className="fadeInUp wow carousal-quote" data-wow-delay=".6s">"Innovation only survives when people believe in their own ideas."</p>
@@ -59,7 +59,7 @@ class Carousal extends Component {
                    </div>
                </div>
                <div className="carousel-item">
-                 <img className="d-block w-100" src="https://s3.amazonaws.com/3c-hackathon/slider/slide3.jpg" alt="Third slide"/>
+                 <img className="d-block w-100" src="https://s3.amazonaws.com/3c-hackathon/slider/inv3.jpg" alt="Third slide"/>
                  <div className="carousel-caption d-md-block">
                       <h1 className="wow fadeInUp heading  " data-wow-delay=".6s"><label className="ccc">3C Logic</label> <label className="innovation">inNovatiON</label></h1>
                       <p className="fadeInUp wow carousal-quote" data-wow-delay=".6s">"The heart and soul of the company is creativity and innovation."</p>
