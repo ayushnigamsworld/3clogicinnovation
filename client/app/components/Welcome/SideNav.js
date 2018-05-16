3c2182083c3d6ea7664521cc44f5afe3888fc0c5
Ayush Nigam <ayushnigamsworld@gmail.com>
2018-05-16 13:38:55 +0530

    Added current selected tab

--- client/app/components/Welcome/SideNav.js
+++ client/app/components/Welcome/SideNav.js
@@ -4,7 +4,19 @@ class SideNav extends Component {
     constructor(props){
         super(props);
         console.log(props);
-        this.state = {loggedInUser: props.loggedInUser};
+        this.state = 
+        {
+            loggedInUser: props.loggedInUser,
+            activeTab : 'RULES'
+        };
+        this.changeCurrentTab = this.changeCurrentTab.bind(this);
+    }
+
+    changeCurrentTab(currentTab) {
+
+        this.setState({
+            activeTab : currentTab
+        });
     }
 
     render() {
@@ -24,16 +36,16 @@ class SideNav extends Component {
 
                 <ul className="list-unstyled components">
                   <b><p><label className="ccc">#3C</label>&nbsp;<label className="innovation">InNovatiON</label></p></b>
-                    <li className="active">
+                    <li className={(this.state.activeTab == 'IDEAS') ? 'active' : '' } onClick={() => { this.changeCurrentTab('IDEAS') }}>
+
                         <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">Ideas</a>
                         <ul className="collapse list-unstyled" id="homeSubmenu">
-
                             <li><a hidden={!(this.props.loggedInUser && this.props.loggedInUser.role === 'ROLE_ADMIN')} onClick={() => {this.props.currentTab('ALL_IDEAS')}}> All Ideas</a></li>
                             <li><a onClick={() => {this.props.currentTab('SUBMIT_IDEA')}}>Add New Idea</a></li>
                             <li><a onClick={() => {this.props.currentTab('APPROVED_IDEAS')}}>Approved Ideas</a></li>
                             <li><a onClick={() => {this.props.currentTab('MY_IDEAS')}}>My Ideas</a></li>
-
                         </ul>
+
                     </li>
                     <li hidden={true}>
                         <a onClick={() => {this.props.currentTab('TEAMS')}} href="#teamSubmenu" data-toggle="collapse" aria-expanded="false">Teams</a>
@@ -47,9 +59,15 @@ class SideNav extends Component {
                             <li><a onClick={() => {this.props.currentTab('TEAM_7')}}>Team 7</a></li>
                         </ul>
                     </li>
-                    <li><a onClick={() => {this.props.currentTab('JUDGES')}} aria-expanded="false">Judges</a></li>
-                    <li><a onClick={() => {this.props.currentTab('RULES')}}  aria-expanded="false">Event Details</a></li>
-                    <li><a onClick={() => {this.props.currentTab('RESOURCE_POOL')}}  aria-expanded="false">List of Resources</a></li>
+                    <li className={(this.state.activeTab == 'JUDGES') ? 'active' : '' }>
+                        <a onClick={() => { this.props.currentTab('JUDGES'); this.changeCurrentTab('JUDGES')  }} aria-expanded="false">Judges</a>
+                    </li>
+                    <li className={(this.state.activeTab == 'RULES') ? 'active' : '' }>
+                        <a onClick={() => { this.props.currentTab('RULES'); this.changeCurrentTab('RULES') }}  aria-expanded="false">Event Details</a>
+                    </li>
+                    <li className={(this.state.activeTab == 'RESOURCE_POOL') ? 'active' : '' }>
+                        <a onClick={() => { this.props.currentTab('RESOURCE_POOL'); this.changeCurrentTab('RESOURCE_POOL') }}  aria-expanded="false">List of Resources</a>
+                    </li>
                     <li><a onClick={() => {this.props.currentTab('MOVE_TO_HOME')}}  aria-expanded="false">Back to home</a></li>
                 </ul>
             </nav>
