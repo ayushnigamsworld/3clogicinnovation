ce9ce846af9c9d36165d3c759eb56d5fb81f593c
mkumar <mkumar@3clogic.com>
2018-05-17 17:06:03 +0530

    resources

--- client/app/components/Welcome/Resources.js
+++ client/app/components/Welcome/Resources.js
@@ -5,29 +5,49 @@ class Resources extends Component {
         return(
             <div id="content">
               <div class="container table-group">
-              Each team should have maximum of 2 resources and minimum 1 resource from each group.
-
+              Each team should have maximum 2 and minimum 1 resource from each group.
               <table class="table">
                 <tr>
                   <td>
                     <table class="table table-bordered table-hover span2">
                            <thead><tr><th>Tech Army</th></tr></thead>
                            <tbody>
+                             <tr><td>Deepak Kumar</td></tr>
+                             <tr><td>Gomtesh Jain</td></tr>
+                             <tr><td>Shahnawaj Khan</td></tr>
                              <tr><td>Aditya Mishra</td></tr>
-                             <tr><td>Ajay Kumar Mehar</td></tr>
                              <tr><td>Anita Yadav</td></tr>
                              <tr><td>Ayush Nigam</td></tr>
+                             <tr><td>Devendra tiwari</td></tr>
+                             <tr><td>Krishna Nand Ranjan</td></tr>
+                             <tr><td>Manoj Kumar</td></tr>
+                             <tr><td>Nishant Thapliyal</td></tr>
+                             <tr><td>Pradeep Mishra</td></tr>
+                             <tr><td>Rohit Gupta</td></tr>
+                             <tr><td>Shailja Tiwari</td></tr>
+                             <tr><td>Shalini Verma</td></tr>
+                             <tr><td>Sumit Chopra</td></tr>
+                             <tr><td>Tripti Bansal</td></tr>
+                             <tr><td>Ajay Kumar Mehar</td></tr>
+                             <tr><td>Nikhil Agrawal</td></tr>
+                             <tr><td>Rajit Sharma</td></tr>
+                             <tr><td>Aayush Singh</td></tr>
+                             <tr><td>Rajat Nigam</td></tr>
                            </tbody>
                     </table>
                   </td>
                   <td>
                     <table class="table table-bordered table-hover span2">
-                        <thead><tr><th>Tech Warriors</th></tr></thead>
+                        <thead><tr><th>Dynamic Dudes</th></tr></thead>
                         <tbody>
-                          <tr><td>Option 1</td></tr>
-                          <tr><td>Option 2</td></tr>
-                          <tr><td>Option 3</td></tr>
-                          <tr><td>Option 4</td></tr>
+                          <tr><td>Damini Singhal</td></tr>
+                          <tr><td>Pulkit Gupta</td></tr>
+                          <tr><td>Nidhi</td></tr>
+                          <tr><td>Ayush Goel</td></tr>
+                          <tr><td>Kamlesh jaiswal</td></tr>
+                          <tr><td>Nishita Jain</td></tr>
+                          <tr><td>Sagar Nandrajog</td></tr>
+                          <tr><td>Meenakshi Das</td></tr>
                         </tbody>
                     </table>
                   </td>
@@ -39,10 +59,17 @@ class Resources extends Component {
                   <table class="table table-bordered table-hover span2">
                       <thead><tr><th>Tech Connect</th></tr></thead>
                       <tbody>
-                        <tr><td>Ayush Goel</td></tr>
-                        <tr><td>Option 2</td></tr>
-                        <tr><td>Option 3</td></tr>
-                        <tr><td>Option 4</td></tr>
+                        <tr><td>Devinder Kumar</td></tr>
+                        <tr><td>Hitesh Sharma</td></tr>
+                        <tr><td>Manali Khare</td></tr>
+                        <tr><td>Rishikesh Himanshu</td></tr>
+                        <tr><td>Somya Agarwal</td></tr>
+                        <tr><td>Vikas Bhadwal</td></tr>
+                        <tr><td>Sasmita panda</td></tr>
+                        <tr><td>Aditya Sharma</td></tr>
+                        <tr><td>Ritesh Chopra</td></tr>
+                        <tr><td>Sandeep Uppal</td></tr>
+                        <tr><td>Ravi Shankar</td></tr>
                       </tbody>
                   </table>
                   </td>
@@ -50,10 +77,14 @@ class Resources extends Component {
                   <table class="table table-bordered table-hover span2">
                       <thead><tr><th>Technocrats</th></tr></thead>
                       <tbody>
-                        <tr><td>Aditya Sharma</td></tr>
-                        <tr><td>Option 2</td></tr>
-                        <tr><td>Option 3</td></tr>
-                        <tr><td>Option 4</td></tr>
+                        <tr><td>Dheraj Kumar</td></tr>
+                        <tr><td>Kuldeep Yadav</td></tr>
+                        <tr><td>Lokesh Kumar</td></tr>
+                        <tr><td>Naveen Verma</td></tr>
+                        <tr><td>Sneha Bhadula</td></tr>
+                        <tr><td>Udham Singh</td></tr>
+                        <tr><td>Vishal Deep Gupta</td></tr>
+                        <tr><td>Himanshu Srivastava</td></tr>
                       </tbody>
                   </table>
                   </td>
@@ -63,12 +94,9 @@ class Resources extends Component {
                 <tr>
                   <td>
                   <table class="table table-bordered table-hover span2">
-                      <thead><tr><th>Dynamic Dudes</th></tr></thead>
+                      <thead><tr><th>Tech Warriors</th></tr></thead>
                       <tbody>
-                        <tr><td>Damini Singhal</td></tr>
-                        <tr><td>Option 2</td></tr>
-                        <tr><td>Option 3</td></tr>
-                        <tr><td>Option 4</td></tr>
+
                       </tbody>
                   </table>
                   </td>
@@ -76,10 +104,7 @@ class Resources extends Component {
                   <table class="table table-bordered table-hover span2">
                       <thead><tr><th>Tech Thalaivars</th></tr></thead>
                       <tbody>
-                        <tr><td>Ajay Kumar Meher</td></tr>
-                        <tr><td>Gomtesh Jain</td></tr>
-                        <tr><td>Pradeep Mishra</td></tr>
-                        <tr><td>Rajit Sharma</td></tr>
+
                       </tbody>
                   </table>
                   </td>
