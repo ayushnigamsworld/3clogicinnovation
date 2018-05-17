be3f97e4addb54ec0dee86bba69e9156c59ffea0
mkumar <mkumar@3clogic.com>
2018-05-17 13:56:21 +0530

    resources list

--- client/app/components/Welcome/Resources.js
+++ client/app/components/Welcome/Resources.js
@@ -4,7 +4,88 @@ class Resources extends Component {
     render() {
         return(
             <div id="content">
-                Hello I am resource page..
+              <div class="container table-group">
+              Each team should have maximum of 2 resources and minimum 1 resource from each group.
+
+              <table class="table">
+                <tr>
+                  <td>
+                    <table class="table table-bordered table-hover span2">
+                           <thead><tr><th>Tech Army</th></tr></thead>
+                           <tbody>
+                             <tr><td>Aditya Mishra</td></tr>
+                             <tr><td>Ajay Kumar Mehar</td></tr>
+                             <tr><td>Anita Yadav</td></tr>
+                             <tr><td>Ayush Nigam</td></tr>
+                           </tbody>
+                    </table>
+                  </td>
+                  <td>
+                    <table class="table table-bordered table-hover span2">
+                        <thead><tr><th>Tech Warriors</th></tr></thead>
+                        <tbody>
+                          <tr><td>Option 1</td></tr>
+                          <tr><td>Option 2</td></tr>
+                          <tr><td>Option 3</td></tr>
+                          <tr><td>Option 4</td></tr>
+                        </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+              <table class="table">
+                <tr>
+                  <td>
+                  <table class="table table-bordered table-hover span2">
+                      <thead><tr><th>Tech Connect</th></tr></thead>
+                      <tbody>
+                        <tr><td>Ayush Goel</td></tr>
+                        <tr><td>Option 2</td></tr>
+                        <tr><td>Option 3</td></tr>
+                        <tr><td>Option 4</td></tr>
+                      </tbody>
+                  </table>
+                  </td>
+                  <td>
+                  <table class="table table-bordered table-hover span2">
+                      <thead><tr><th>Technocrats</th></tr></thead>
+                      <tbody>
+                        <tr><td>Aditya Sharma</td></tr>
+                        <tr><td>Option 2</td></tr>
+                        <tr><td>Option 3</td></tr>
+                        <tr><td>Option 4</td></tr>
+                      </tbody>
+                  </table>
+                  </td>
+                </tr>
+              </table>
+              <table class="table">
+                <tr>
+                  <td>
+                  <table class="table table-bordered table-hover span2">
+                      <thead><tr><th>Dynamic Dudes</th></tr></thead>
+                      <tbody>
+                        <tr><td>Damini Singhal</td></tr>
+                        <tr><td>Option 2</td></tr>
+                        <tr><td>Option 3</td></tr>
+                        <tr><td>Option 4</td></tr>
+                      </tbody>
+                  </table>
+                  </td>
+                  <td>
+                  <table class="table table-bordered table-hover span2">
+                      <thead><tr><th>Tech Thalaivars</th></tr></thead>
+                      <tbody>
+                        <tr><td>Ajay Kumar Meher</td></tr>
+                        <tr><td>Gomtesh Jain</td></tr>
+                        <tr><td>Pradeep Mishra</td></tr>
+                        <tr><td>Rajit Sharma</td></tr>
+                      </tbody>
+                  </table>
+                  </td>
+                </tr>
+              </table>
+                 </div>
             </div>
         );
     }
